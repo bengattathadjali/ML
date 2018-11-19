ffa4014a134b6f745cfdcabff23184dba6bd4db0
alibengattat <bengattathadjali@gmail.com>
2018-11-19 23:22:53 +0100

    2322

--- readfile.py
+++ readfile.py
@@ -1,11 +1,21 @@
 f=open("result.sh","r")
 lines=f.readlines()
-i=1
-while i<100000:
-    print (lines[i])
-    print(i)
-    i+=89
+i=lines[77].index("ROC")
+j=80
 
+
+lndex = 'Weighted Avg'
+rocmax =0
+with open("result.sh","r") as myFile:
+    for num, line in enumerate(myFile, 1):
+        if lndex in line:
+            roc =float(lines[num-1][i:i+5])
+            if roc>rocmax:
+                rocmax=roc
+                print(num)
+                
+                
+print (rocmax)        
 
 f.close()
 
