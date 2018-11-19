a14d24f02aa4e832fc446f7f60107c1edf1ed716
alibengattat <bengattathadjali@gmail.com>
2018-11-19 23:36:16 +0100

    2336

--- Exe_File.py
+++ Exe_File.py
@@ -3,8 +3,8 @@ import os
 c = 0.01
 m = 2
 
-weka_path = "java -cp '/home/eliot/Bureau/app machine/weka-3-8-1' "
-weka_desc_path = "'/home/eliot/Bureau/app machine/tp-projet/data/features/train/y_features_opt2_300_train____________.csv'  "
+weka_path = "java -cp 'weka-3-8-1' "
+weka_desc_path = "'train/y_features_opt2_300_train____________.csv'  "
 text_write = ">> result.sh"
 
 while c<1:
@@ -13,12 +13,6 @@ while c<1:
         os.system(weka_path+ ('weka.classifiers.trees.J48 -C +%f -M +%d -t ' %(c,m)) +weka_desc_path+text_write)
         m+=1
     c+= 0.1
- 
-    
-
-    
-
-   
 
 
 
