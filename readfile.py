00f777d3c9225d1f3458daa5f4d23050a022b923
alibengattat <bengattathadjali@gmail.com>
2018-11-20 02:21:13 +0100

    0221

--- readfile.py
+++ readfile.py
@@ -1,21 +1,35 @@
 f=open("result.sh","r")
 lines=f.readlines()
 i=lines[77].index("ROC")
-j=80
+c=lines[1].index("-C")
+
+#value_c=float(lines[1][c+4:c+10])
+j=0
 
 
 lndex = 'Weighted Avg'
+index_2 = 'Options:'
 rocmax =0
 with open("result.sh","r") as myFile:
     for num, line in enumerate(myFile, 1):
         if lndex in line:
-            roc =float(lines[num-1][i:i+5])
-            if roc>rocmax:
-                rocmax=roc
-                print(num)
+            if j==1:
+                roc =float(lines[num-1][i:i+5])
+                j=0
+                if roc>rocmax:
+                    rocmax=roc
+                    line_avg=num
+                    while index_2 in line:
+                        line_avg-=1
+                        print(line_avg)
+                    #value_c=float(lines[num-79][c+4:c+10])
+            else:
+                j+=1
+            
                 
-                
-print (rocmax)        
+              
+print (rocmax)  
+      
 
 f.close()
 
--- script.sh
+++ script.sh
@@ -32,3 +32,24 @@ java -cp '/home/eliot/Bureau/weka-3-8-1/weka.jar' weka.classifiers.trees.J48 -C 
 /home/eliot/Bureau/app\ machine/tp-projet/data/features/train/y_features_opt2_300_train____________.csv > /home/eliot/Bureau/test2.sh
 ((c++))
 done
+
+f=open("result.sh","r")
+lines=f.readlines()
+i=lines[77].index("ROC")
+j=80
+
+
+lndex = 'Weighted Avg'
+rocmax =0
+with open("result.sh","r") as myFile:
+    for num, line in enumerate(myFile, 1):
+        if lndex in line:
+            roc =float(lines[num-1][i:i+5])
+            if roc>rocmax:
+                rocmax=roc
+                print(num)
+                
+                
+print (rocmax)        
+
+f.close()
