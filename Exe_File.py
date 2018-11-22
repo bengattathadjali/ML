eaa20b0e18f0b0a7f5e19a00d03c5f838069f820
alibengattat <bengattathadjali@gmail.com>
2018-11-22 19:32:55 +0100

    Over

--- Exe_File.py
+++ Exe_File.py
@@ -8,8 +8,8 @@ weka_desc_path = " 'train/y_features_opt2_300_train.csv' "
 text_write = '>> result.sh'
 #os.system("java -cp 'weka-3-8-1/weka.jar' weka.classifiers.trees.J48 -C 0.2 -M 2 -t 'train/y_features_opt2_300_train.csv'")
 while c<1:
-    m=1
-    while m<10:
+    m=10
+    while m<20:
         os.system(weka_path+('weka.classifiers.trees.J48 -C +%f -M +%d -t' %(c,m)) +weka_desc_path+text_write)
         m+=1
         print(c,m)
