8e0a4d9d3eac4ae3a970d0ea7e1fc98ecbaf21e3
bengattathadjali <bengattathadjali@gmail.com>
2018-11-30 17:22:30 +0100

    Lab

--- Exe_File_RF.py
+++ Exe_File_RF.py
@@ -7,19 +7,19 @@ P = 1
 weka_path = "java -cp 'weka-3-8-1/weka.jar' "
 weka_desc_path = " 'train/y_features_opt2_300_train.csv' "
 text_write = '>> result_RF.sh'
-while P<10:
+while P<100:
     I = 1
-    while I<10:
+    while I<100:
         NumSlots = 1
-        while NumSlots<10:
+        while NumSlots<100:
             K = 0
-            while K<10:
+            while K<100:
                 M = 1
-                while M<10:
+                while M<100:
                     V = 0.1
                     while V<1:
                         S = 1
-                        while S<10:
+                        while S<100:
                             os.system(weka_path+('weka.classifiers.trees.RandomForest -P %d -I %d -num-slots %d -K %d -M %d -V %f -S %d -t' %(P,I,NumSlots,K,M,V,S)) +weka_desc_path+text_write)
                             print("P :%d | I :%d | numSlots :%d | K :%d | M :%d | V :%f | S :%d" %(P,I,NumSlots,K,M,V,S))
                             S+=1
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-Mounir
+TP apprantissage machine : 
+Exe_file_RF.py : script qui appel weka avec des boucles imbriqués pour chaque paramétre de l'algorithme RandomForest
+read_file : script qui affiche les meilleurs paramètres a partir du fichier result_RF.sh avec :
+		-Options : (tous les paramètres de l'algorithme)
+		-Les valeurs ROC Precision Recall F-Mesure
+		-confusion matrix
+
+
+Ce TP a été fait par :
+
+		-BENHALIMA MOUNIR
+		-BENGATTAT HADJ ALI
