9489cbd9b0a78c73f11d14621fdb6976cdcea1d9
bengattathadjali <bengattathadjali@gmail.com>
2018-12-02 00:06:04 +0100

    modify J48

--- Exe_File_J48_Y.py
+++ Exe_File_J48_Y.py
@@ -8,7 +8,7 @@ weka_desc_path = " 'train/y_features_opt2_300_train.csv' "
 text_write = '>> result.sh'
 #os.system("java -cp 'weka-3-8-1/weka.jar' weka.classifiers.trees.J48 -C 0.2 -M 2 -t 'train/y_features_opt2_300_train.csv'")
 while c<1:
-    m=20
+    m=1
     while m<30:
         os.system(weka_path+('weka.classifiers.trees.J48 -C +%f -M +%d -t' %(c,m)) +weka_desc_path+text_write)
         m+=1
